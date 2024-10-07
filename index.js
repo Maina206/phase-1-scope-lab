934d0f0582bb34a730b4da406c6f665c08597e76
Alexander <maina206mwangi@gmail.com>
2024-10-07 23:28:24 +0300

    JS scope completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+  }
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(name) {
+    window.bestCustomer = name;
+  }
+
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value';
+}
